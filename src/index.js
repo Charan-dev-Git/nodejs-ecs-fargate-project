20186cbf5acfdd0593e9e57d5dd1badd2b54c36e
Charan P <charanpadmanaban.official@gmail.com>
2026-04-23 23:30:35 +0530

    Test CI/CD automatic deployment

--- src/index.js
+++ src/index.js
@@ -4,7 +4,7 @@ const app = express();
 const PORT = process.env.PORT || 3000;
 
 app.get("/", (req, res) => {
-  res.send("Node.js ECS Fargate App is running");
+  res.send("App updated from GitHub Actions CI/CD");;
 });
 
 app.get("/health", (req, res) => {
