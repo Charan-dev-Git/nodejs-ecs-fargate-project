7e87e501929ec23ea765903fe88957b0b2bcddf1
Charan P <charanpadmanaban.official@gmail.com>
2026-04-24 07:52:19 +0530

    Update index.js

--- src/index.js
+++ src/index.js
@@ -4,7 +4,7 @@ const app = express();
 const PORT = process.env.PORT || 3000;
 
 app.get("/", (req, res) => {
-  res.send("App updated from GitHub Actions charan CI/CD");;
+  res.send("App updated from GitHub Actions CI/CD");;
 });
 
 app.get("/health", (req, res) => {
